4a0c394c7c0ef2a7b0261b07912a49ecd57c23db
DK\adesh.kumar <adesh.kumar@valtech.com>
2018-01-05 17:59:14 +0100

    first commit

---
+++ AkqaTest/AKQATestUnitTest/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AkqaTest;
+using System.Web;
+
+namespace AKQATestUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void AmountInWords()
+        {
+            // Arrange
+            string name = "AKQA TEST";
+            string amount = "1452.23";
+            string amountinwords = "Name - AKQA TEST | Amount - 1452.23  | Amount in words - One Thousand Four Hundred  Fifty-Two dollars Twenty-Three cents";
+            int wordcount = amountinwords.Length;
+            int testresultCount;
+            
+            //Act
+            AKQA obj=new AKQA();
+            var result = obj.GetPersonData(name, amount);
+            testresultCount = result.Length;
+
+            //Assert
+            Assert.AreEqual(wordcount, testresultCount);
+        }
+    }
+}
